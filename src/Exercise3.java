40d84e12d11f77f70ee6e3fd615ba081d538b1c5
AlisonLN <alison.nascimento@hotmail.com>
2023-06-16 15:01:00 -0300

    atualizando

--- src/Exercise3.java
+++ src/Exercise3.java
@@ -24,14 +24,14 @@ public class Exercise3 {
         // Professor Fez :
 
 
-        double R, A, pi = 3.14159;
-
-        R = sc.nextDouble();
-
-        A = pi * R * R;
-
-        System.out.printf("A=%.4f%n", A);
-
-
+//        double R, A, pi = 3.14159;
+//
+//        R = sc.nextDouble();
+//
+//        A = pi * R * R;
+//
+//        System.out.printf("A=%.4f%n", A);
+//
+//
     }
 }
--- src/Exercise6.java
+++ src/Exercise6.java
@@ -32,6 +32,22 @@ public class Exercise6 {
 
 
 
+        //Professor Fez
+//
+//        int cod1, cod2, qte1, qte2;
+//        double preco1, preco2, total;
+//
+//        cod1 = sc.nextInt();
+//        qte1 = sc.nextInt();
+//        preco1 = sc.nextDouble();
+//
+//        cod2 = sc.nextInt();
+//        qte2 = sc.nextInt();
+//        preco2 = sc.nextDouble();
+//
+//        total = preco1 * qte1 + preco2 * qte2;
+//
+//        System.out.printf("VALOR A PAGAR: R$ %.2f%n", total);
 
 
 
